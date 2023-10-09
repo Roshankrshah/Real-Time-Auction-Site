d4bfaa09915b86fe783f34f43e9d6bfeaff0945f
Roshan <shahroshan0720@gmail.com>
2023-10-09 14:01:52 +0530

    upload image route setup

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,15 @@ const connectDB = require('./db/config');
 const auth = require('./routes/auth');
 const user = require('./routes/user');
 const ad = require('./routes/ad');
+const upload = require('./routes/upload');
 const app = express();
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
 
 app.use(express.json());
 
@@ -22,6 +30,7 @@ app.get('/',(req,res)=>{
 app.use('/auth',auth);
 app.use('/user',user);
 app.use('/ad',ad);
+app.use('/upload',upload);
 
 const PORT = process.env.PORT || 4444;
 
---
+++ Server/routes/upload.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cloudinary = require('cloudinary').v2;
+const router = express.Router();
+
+const { upload } = require('../utils/fileUpload');
+const isAuth = require('../middlewares/isAuth');
+
+
+router.post('/image',isAuth,upload.single('image'),async(req,res)=>{
+    const file = req.file;
+    try{
+        const result = await cloudinary.uploader.upload(file.path,{
+            folder: "Auction",
+            resource_type: "image"
+        });
+        res.status(201).json({imagePath: result.secure_url});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({errors: [{msg: 'Server error'}]});
+    }
+})
+
+module.exports = router;
