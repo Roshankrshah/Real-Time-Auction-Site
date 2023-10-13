e10a366bb270d19ed5f87d22354db7e016b38b10
Roshan <shahroshan0720@gmail.com>
2023-10-13 13:43:36 +0530

    btns added

--- Client/homePage/adDetail.js
+++ Client/homePage/adDetail.js
@@ -1,23 +1,28 @@
 const productImageDom = document.querySelector('.product-image');
 const productDetailDom = document.querySelector('.product-details');
+const dynamicContainer = document.querySelector('.dynamic-container');
+const modifyDom = document.querySelector('.modify-btns');
 
 const adId = window.location.href.split('?')[1].split('=')[1];
 
 console.log(adId);
 
-const start = async()=>{
-    const res = await fetch(`http://localhost:4444/ad/${adId}`,{
+const start = async () => {
+    const res = await fetch(`http://localhost:4444/ad/${adId}`, {
         headers: {
             'x-auth-token': localStorage.getItem('token')
         }
     });
 
     const resData = await res.json();
-    console.log(resData);
-    productImageDom.innerHTML = `
+    if (res.status >= 400) {
+        alert(resData.errors[0].msg);
+    } else {
+        console.log(resData);
+        productImageDom.innerHTML = `
         <h2>${resData.productName}</h2>
         <img src="${resData.image}" alt="Product Image">`;
-    productDetailDom.innerHTML = `
+        productDetailDom.innerHTML = `
         <h2>Product Details</h2>
         <p><strong>Description</strong><br> ${resData.description}</p>
         <p><strong>Info</strong><br> Posted on: ${new Date(resData.updatedAt).toLocaleString()}
@@ -25,12 +30,35 @@ const start = async()=>{
         <br> Base Price: ₹ ${resData.basePrice.$numberDecimal}</p>
         <p><strong> Auction</strong><br>
         Status: ${resData.auctionStarted === false ?
-            'Upcoming' : ad.auctionEnded === false ?
-                'Ingoing' : 'Completed'}
+                'Upcoming' : ad.auctionEnded === false ?
+                    'Ingoing' : 'Completed'}
         <br>Bids: ${resData.bids.length}
         <br>Time remaining: 
         <br>Current Price: ₹ ${resData.currentPrice.$numberDecimal}
         <br> Current Bidder: </p>`
+    }
+
+    const user = await fetch(`http://localhost:4444/auth`, {
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        }
+    })
+    const userData = await user.json();
+    if (user.status >= 400) {
+        alert(userData.errors[0].msg);
+    } else {
+        if(userData.user._id == resData.owner._id){
+            dynamicContainer.innerHTML = `<button type="button" class="btn btn-primary btn-lg">Start Auction</button>`;
+            modifyDom.innerHTML = `
+            <button type="button" class="btn btn-primary ">Update</button>
+            <button type="button" class="btn btn-danger">Delete</button>`;
+        }else{
+            dynamicContainer.innerHTML = `
+            <input type="number" class="input-field" placeholder="Enter the price(₹)">
+            <button type="button" class="btn btn-primary">Placed Bid</button>`;
+        }
+    }
+
 }
 
 start();
